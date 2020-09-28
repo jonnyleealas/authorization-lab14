e7f319ca50c21c4795264334f5075b0badf44b6f
jonathon lee <jonnyleealas@gmail.com>
2020-09-28 00:32:48 -0700

    testing4

--- _test_/server.test.js
+++ _test_/server.test.js
@@ -1,17 +1,17 @@
 'use strict';
 
-const supergoose = require('@code-fellows/supergoose');
-const request = require('superagent');
 require('dotenv').config();
-const jwt = require('jsonwebtoken');
 const server = require('../server.js');
-const req = supergoose(server.app);
+const supergoose = require('@code-fellows/supergoose');
+// const request = require('superagent');
+const jwt = require('jsonwebtoken');
+const requiregoose = supergoose(server.app);
 
 
 describe('Basic error testing', () => {
 
     it('Should give a 404 error on a bad route', async() => {
-      let res = await req.get('/bad');
+      let res = await requiregoose.get('/bad');
       expect(res.status).toEqual(404);
     })
   });
@@ -24,7 +24,7 @@ describe('Test Auth Router', () => {
             'password': 'serra',
             'role': 'regular'
         }
-        let res = await req.post('/signup').send(obj);
+        let res = await requiregoose.post('/signup').send(obj);
         let result = res.body;
         expect(result.user.username).toBe(obj.username);
         expect(result.user.password).toBeDefined();
@@ -44,7 +44,7 @@ describe('Test auth routes', () => {
         role: 'regular'
       }
   
-      let res = await req.post('/signup').send(obj);
+      let res = await requiregoose.post('/signup').send(obj);
       expect(res.body.user.username).toEqual('sally');
       expect(res.body.user.role).toEqual('regular');
       expect(res.body.user).toBeTruthy();
@@ -62,8 +62,8 @@ describe('Testing signin', () => {
       password: "jessy",
     }
 
-    let res = await req.post('/signin').auth("sally", "jessy");
-    expect(res.body.token).toBeTruthy();
-    expect(res.status).toEqual(200);
+    let result = await requiregoose.post('/signin').auth("sally", "jessy")
+    expect(result.body.token).toBeTruthy()
+    expect(result.status).toEqual(200)
   })
-});
+})
