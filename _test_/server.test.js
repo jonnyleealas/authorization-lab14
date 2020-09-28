f1f25aab1eb4dbf85517cea7d11b6776d143e991
jonathon lee <jonnyleealas@gmail.com>
2020-09-28 00:27:19 -0700

    testing3

--- _test_/server.test.js
+++ _test_/server.test.js
@@ -3,7 +3,6 @@
 const supergoose = require('@code-fellows/supergoose');
 const request = require('superagent');
 require('dotenv').config();
-const cors = require('cors')
 const jwt = require('jsonwebtoken');
 const server = require('../server.js');
 const req = supergoose(server.app);
@@ -12,35 +11,28 @@ const req = supergoose(server.app);
 describe('Basic error testing', () => {
 
     it('Should give a 404 error on a bad route', async() => {
-  
       let res = await req.get('/bad');
       expect(res.status).toEqual(404);
-      
-    });
-    
+    })
   });
 
 describe('Test Auth Router', () => {
 
     it('Should create a new user', async () => {
         let obj = {
-            'username': 'Jon',
-            'password': 'Jon',
+            'username': 'michael',
+            'password': 'serra',
             'role': 'regular'
         }
         let res = await req.post('/signup').send(obj);
-        let output = res.body;
-        expect(output.user.username).toBe(obj.username);
-        expect(output.user.password).toBeDefined();
-        expect(output.user.role).toBe(obj.role);
-        expect(output.token).toBeDefined();
+        let result = res.body;
+        expect(result.user.username).toBe(obj.username);
+        expect(result.user.password).toBeDefined();
+        expect(result.user.role).toBe(obj.role);
+        expect(result.token).toBeDefined();
         expect(res.status).toEqual(200);
-
-
     })
-
-
-})
+});
 
 describe('Test auth routes', () => {
 
@@ -50,28 +42,28 @@ describe('Test auth routes', () => {
         username: 'sally',
         password: 'jessy',
         role: 'regular'
-      };
+      }
   
       let res = await req.post('/signup').send(obj);
-      
-      expect(res.body.user.role).toEqual('regular');
-      expect(res.body.token).toBeTruthy();
-      expect(res.body.user).toBeTruthy();
       expect(res.body.user.username).toEqual('sally');
+      expect(res.body.user.role).toEqual('regular');
+      expect(res.body.user).toBeTruthy();
+      expect(res.body.token).toBeTruthy();
       expect(res.status).toEqual(200);
-      
-    });
-    it('POST /signin with basic authentication headers logs in a user and sends an object with the user and the token to the client ', async() => {
+    })
+});
 
-        let obj = {
-          username: "sally",
-          password: "jessy",
-        };
-    
-        let res = await req.post('/signin').auth("sally", "jessy");
-        expect(res.body.token).toBeTruthy();
-        expect(res.status).toEqual(200);
-        
-      });
+describe('Testing signin', () => {
 
-})
+  it('Should sign in and return status 200 ', async() => {
+
+    let obj = {
+      username: "sally",
+      password: "jessy",
+    }
+
+    let res = await req.post('/signin').auth("sally", "jessy");
+    expect(res.body.token).toBeTruthy();
+    expect(res.status).toEqual(200);
+  })
+});
